95c57ac189f1f53e5688b82b1f3c1c26427a99d1
unhochoi <yms04089@kookmin.ac.kr>
2022-03-08 17:29:14 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# 필요 패키지 다운로드
 sudo yum update -y
 sudo yum install git -y
 sudo yum install python3-pip -y
-
 sudo pip3 install virtualenv
 
 # Git Clone 및 접근
@@ -11,14 +11,15 @@ cd ~
 git clone https://github.com/unhochoi/flask-inference.git
 cd flask-inference
 
-# 가상 환경 생성
+# venv 라는 이름의 가상 환경 생성
 virtualenv venv
+# venv 가상 환경 활성화
 . venv/bin/activate
 
-# 가상 환경 내부에 필요 Library 다운로드
+# 가상 환경 내부에 필요 패키지 다운로드
 pip3 install -r requirements.txt
 
-# Main.service 실행
+# Flask Application 실행
 sudo cp main.service /etc/systemd/system/
 sudo systemctl start main
 sudo systemctl enable main
