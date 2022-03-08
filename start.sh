f0c7d587b5fc9057527a8de9f9d5193f08419502
unhochoi <yms04089@kookmin.ac.kr>
2022-03-08 19:43:33 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,9 +11,9 @@ virtualenv venv
 . venv/bin/activate
 
 # 가상 환경 내부에 필요 패키지 다운로드
-pip3 install -r /flask-inference/requirements.txt
+pip3 install -r /home/hadoop/flask-inference/requirements.txt
 
 # Flask Application 실행
-sudo cp /flask-inference/main.service /etc/systemd/system/
+sudo cp /home/hadoop/flask-inference/main.service /etc/systemd/system/
 sudo systemctl start main
 sudo systemctl enable main
