b7381391b528da1333a8191d06bdf4736f4a071a
unhochoi <yms04089@kookmin.ac.kr>
2022-03-08 19:56:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Import package
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
 import numpy as np
 import tensorflow as tf
@@ -15,11 +15,36 @@ minmax_scaler = pickle.load(open('/home/hadoop/dos/dos/scaler/minmax_scaler.pkl'
 
 app = Flask(__name__)
 
-@app.route('/')
-def basic():
+@app.route('/', methods=['POST'])
+def inference():
     # request를 json 형식으로 변수에 할당
     param = request.get_json()
-    return "hello"
+
+    # Preprocess features from events
+    lr = param["lr"]
+    lc = param["lc"]
+    rc = param["rc"]
+    ld = param["ld"]
+    rd = param["rd"]
+    lnnz = param["lnnz"]
+    rnnz = param["rnnz"]
+
+    # Create input feature to use as model input
+    input_feature = np.array([[lr,lc,rc,ld,rd,lnnz,rnnz]])
+    
+    # Apply minmax scaler to input_feature
+    input_feature_scaler = minmax_scaler.transform(input_feature)
+
+    # Generate model-specific predictions for input feature
+    smsm_dnn_result = smsm_dnn_model.predict(input_feature_scaler)
+    smdm_dnn_result = smdm_dnn_model.predict(input_feature_scaler)
+
+    # If sm*dm is better than sm*sm
+    if (smdm_dnn_result[0] <= smsm_dnn_result[0]):
+        return "smdm"
+    # If sm*sm is better than sm*dm
+    else:
+        return "smsm"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80 ,debug=True)
