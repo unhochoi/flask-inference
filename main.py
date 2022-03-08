f7fd341f5c28c0e9649cec533e7e9209efe493aa
unhochoi <yms04089@kookmin.ac.kr>
2022-03-08 19:14:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from flask import Flask
+# Import package
+from flask import Flask, request
+import json
+import numpy as np
+import tensorflow as tf
+import pickle
+from sklearn.preprocessing import MinMaxScaler
 
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions, MobileNetV2
+# Load Model
+smsm_dnn_model = tf.keras.models.load_model('/home/hadoop/dos/dos/model/smsm_dnn_model')
+smdm_dnn_model = tf.keras.models.load_model('/home/hadoop/dos/dos/model/smdm_dnn_model')
+
+# Load Scaler
+minmax_scaler = pickle.load(open('/home/hadoop/dos/dos/scaler/minmax_scaler.pkl','rb'))
 
 app = Flask(__name__)
 
-inference_model = MobileNetV2()
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def basic():
+    # request를 json 형식으로 변수에 할당
+    param = request.get_json()
+    return "hello"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80 ,debug=True)
