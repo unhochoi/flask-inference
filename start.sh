e7e6fa409b2aa6c6db6c10c7ef2d6258497ced3a
unhochoi <yms04089@kookmin.ac.kr>
2022-03-15 16:52:51 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # 필요 패키지 다운로드
-sudo yum update -y
-sudo yum install python3-pip -y
-sudo pip3 install virtualenv
+yum update -y
+yum install python3-pip -y
+pip3 install virtualenv
 
 # venv 라는 이름의 가상 환경 생성
-sudo virtualenv venv
+virtualenv venv
 # venv 가상 환경 활성화
 . venv/bin/activate
 
 python -m pip install --upgrade pip
 
 # 가상 환경 내부에 필요 패키지 다운로드
-pip3 install -r /home/hadoop/flask-inference-in-emr/requirements.txt
+sudo pip3 install -r /home/hadoop/flask-inference-in-emr/requirements.txt
 
-pip3 install tensorflow==2.5.0 --use-feature=2020-resolver
+sudo pip3 install tensorflow==2.5.0 --use-feature=2020-resolver
 
 # Flask Application 실행
 sudo cp /home/hadoop/flask-inference-in-emr/main.service /etc/systemd/system/
